b2f6e960397231601ce4e3ca53c8179ef37aaf00
Zuminick <teodor.pifagorski@icloud.com>
2023-05-26 23:20:48 +0300

    frontend navbar

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+
+<h4>Hello</h4>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index']);
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function() {
 
