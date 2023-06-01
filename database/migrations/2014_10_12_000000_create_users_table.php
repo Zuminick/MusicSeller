be0c088366ccc9174fa1c14cbe2bec37b4e87f63
Zuminick <teodor.pifagorski@icloud.com>
2023-06-01 19:22:10 +0300

    forms and users migrations update references id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->tinyInteger('role_as')->default('0')->comment('0=user, 1=admin');
--- database/migrations/2023_05_25_152325_create_forms_table.php
+++ database/migrations/2023_05_25_152325_create_forms_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('forms', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('post_id');
+            $table->unsignedBigInteger('post_id');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             $table->string('name');
             $table->string('phone');
